b6944db035cb9db8a909b6fa5632837d73c18570
JuhG <juhg24@gmail.com>
2020-03-19 10:02:02 +0100

    test if I can break it

--- app/utils/useAuth.tsx
+++ app/utils/useAuth.tsx
@@ -21,7 +21,7 @@ const defaultState: State = {
 
 const Context = React.createContext({
   state: defaultState,
-  dispatch: (action: Action): State | void => defaultState,
+  dispatch: (action: Action): State => defaultState,
 })
 
 export const AuthProvider = ({
